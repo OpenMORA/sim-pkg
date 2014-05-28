e9e583e2b75c214338cf9b0633f610ffd2e861be
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2014-05-28 12:01:15 +0200

    Fixes to build in Windows

--- MobileRobot_Simul/main.cpp
+++ MobileRobot_Simul/main.cpp
@@ -2,8 +2,6 @@
    |                 Open MORA (MObile Robot Arquitecture)                     |
    +---------------------------------------------------------------------------+ */
 
-#include "CRobotSimulApp.h" // User C++ app class 
-
 #define MORA_APP_CLASS         CRobotSimulApp     
 #define MORA_APP_NAME          "MobileRobot_Simul"    // Default MOOSApp app name
 
